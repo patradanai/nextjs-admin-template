2f81eb5f4f8b8e6fbc2007116d128e6c5b47da10
digio <=>
2022-03-19 14:08:47 +0700

    add icon and ul list

--- src/components/common/layout/sidebar/index.tsx
+++ src/components/common/layout/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
 import { ViewList } from '@components/icons'
 import React, { useState } from 'react'
-import { Calendar, Duplicate, ClipBoard, Graph } from '@components/icons'
+import { Calendar, Duplicate, ClipBoard, Graph, Chat, Document, Gift, Download, Setting } from '@components/icons'
 import { ListMenu } from './components/list-menu'
 
 export const Sidebar: NextPage = () => {
@@ -15,8 +15,8 @@ export const Sidebar: NextPage = () => {
     <aside
       onMouseEnter={handleOpen}
       onMouseLeave={handleOpen}
-      className={`fixed inset-0 h-full motion-reduce:transition-none transition-all ease-in-out delay-150 bg-primary ${
-        isOpen ? 'w-[260px]' : 'w-[68px]'
+      className={`fixed inset-0 h-full overflow-y-auto motion-reduce:transition-none transition-all ease-in-out delay-150 bg-primary ${
+        isOpen ? 'w-[260px]' : 'w-[66px]'
       }`}
     >
       <div className='overflow-hidden'>
@@ -33,13 +33,19 @@ export const Sidebar: NextPage = () => {
         {/* List */}
         <ul className='space-y-3'>
           <ListMenu icons={<Graph className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
-          <ListMenu icons={<Calendar className='w-8 h-8 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
-          <ListMenu icons={<Duplicate className='w-8 h-8 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
-          <ListMenu icons={<ClipBoard className='w-8 h-8 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<Calendar className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<Duplicate className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<ClipBoard className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
 
           <li>SETTINGS</li>
-          <li></li>
+          <ListMenu icons={<Setting className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<Gift className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          
           <li>DOCUMENTS</li>
+          <ListMenu icons={<Chat className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<Document className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<Download className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
+          <ListMenu icons={<Download className='w-6 h-6 text-white' />} name='สถิติการใช้งาน' status={isOpen} />
         </ul>
       </div>
     </aside>
